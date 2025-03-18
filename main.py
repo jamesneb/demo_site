00f8e1cf8e1c826f5de37181005a562999a50cd4
James Nebeker <jnebeker@truckit.com>
2025-03-18 12:56:26 -0700

    endpoints added.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import requests
+
+api_base_url = "https://api.stack.truckit.com/"
+api_sign_in_endpoint = "api/2/signin"
+api_sign_in_url = f"{api_base_url}{api_sign_in_endpoint}"
+simulate_movement_ep = "api/1/demo/simulate-movement"
+api_simulate_movement_url = f"{api_base_url}{simulate_movement_ep}"
+trucks_ep = "api/2/trucks"
+api_trucks_url = f"{api_base_url}{trucks_ep}"
+
+username = "jnebeker"
+password = "welcome"
+
+signin_payload = {"username": username, "password": password}
+response = requests.post(api_sign_in_url, json=signin_payload)
 
